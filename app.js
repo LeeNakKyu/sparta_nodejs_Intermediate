b05f3a93aa2408142b12e3a6a3c1ad9934c58863
LeeNakKyu <nakku1055@naver.com>
2023-11-20 14:42:18 +0900

    comment: 주석 추가

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import express from 'express';
-import { SERVER_PORT } from './constants/app.constant.js';
+import express from 'express';  // ESModule 시스템 사용 시, package.json에서 "type": "module", 추가
+import { SERVER_PORT } from './constants/app.constant.js';  // ./constants/app.constant.js에 있는 SERVER_PORT 접근
 const app = express()
 
 app.get('/', (req, res) => {
--- constants/app.constant.js
+++ constants/app.constant.js
@@ -1,3 +1,3 @@
-import 'dotenv/config';
+import 'dotenv/config'; // dotenv 설치 시 .env에 접근 가능
 
-export const SERVER_PORT = process.env.SERVER_PORT;
+export const SERVER_PORT = process.env.SERVER_PORT; // env에 있는 SERVER_PORT=3000를 가져와서 할당
